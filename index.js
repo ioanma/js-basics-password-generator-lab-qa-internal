79cc64d95110c24c3ee7c5999796b868b4c727ee
Marios Ioannidis <ioanmar80@gmail.com>
2018-08-01 16:47:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 window.addEventListener("load", () => {
-  //start your code here
+  // Random lowercase letter
+  var lower = [
+  "Apples",
+  "Bananas",
+  "Pears"
+];
+
+var randomItem = myArray[Math.floor(Math.random()*myArray.length)];
+  function randomLower() {
+    
+  }
 });
